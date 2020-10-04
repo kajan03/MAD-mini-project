8c9a556eb8cb3a65421f4d69c786fe7ddeb0552d
USER <dinurika.s99@gmail.com>
2020-10-04 12:38:59 +0530

    test2

--- app/src/main/java/com/example/myapplication1/ProfileUser.java
+++ app/src/main/java/com/example/myapplication1/ProfileUser.java
@@ -1,14 +1,20 @@
 package com.example.myapplication1;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -24,6 +30,7 @@ public class ProfileUser extends AppCompatActivity {
     FirebaseFirestore fStore;
     String userId;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +39,7 @@ public class ProfileUser extends AppCompatActivity {
         profEmail = findViewById(R.id.profEmail);
         profPhone = findViewById(R.id.profPhone);
         edit = findViewById(R.id.save);
+
 
         fAuth = FirebaseAuth.getInstance();
         fStore = FirebaseFirestore.getInstance();
@@ -50,6 +58,8 @@ public class ProfileUser extends AppCompatActivity {
 
             }
         });
+
+
         edit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -61,4 +71,8 @@ public class ProfileUser extends AppCompatActivity {
 
 
     }
+
+
+
+
 }
